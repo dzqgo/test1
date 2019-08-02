9eb933e7a4379e9e09f0859a71c8d55aeaa74b56
UID <UID@DESKTOP-DACJOCN>
2019-08-02 09:32:04 +0800

    1

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
@@ -0,0 +1,24 @@
+package com.tedu.sp01.pojo;
+
+import java.util.List;
+
+public class Order {
+
+	public void setId(String orderId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setItems(List<Item> asList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
